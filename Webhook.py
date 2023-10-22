46a92b790df00cfed93052f476e6fe3dcdf32386
Jasper Aden <128797567+jaaden02@users.noreply.github.com>
2023-10-22 22:13:26 +0200

    rolling back

--- Webhook.py
+++ Webhook.py
@@ -5,57 +5,6 @@ from flask import Flask, request, jsonify
 # Initialize Flask app
 app = Flask(__name__)
 
-model= "gpt-3.5-turbo-16k"
-token= 16000
-
-print("Initializing...")
-
-# Role definition
-role = "You are a personal tour guide responsible for creating a detailed, all-inclusive, step-by-step itinerary."
-
-# Static Parameters (Personal Profile, Travel Preferences, etc.)
-static_params = {
-    'age': 21,
-    'gender': 'male',
-    'relationship_status': 'single',
-    'travel_companions': 'none',
-    'travel_type': 'solo',
-    'budget': 'student budget',
-    'activity_level': 'active'
-}
-
-# Interests
-interests = ['independent films', 'techno music', 'vintage items', 'small cafes', 'old rustic bars']
-
-# Varying Parameters (Specific to each trip)
-varying_params = {
-    'destination': 'Berlin',
-    'location': 'near S Warschauer Brücke',
-    'transport': 'comfortable using public transit',
-    'duration': 'Thursday to Sunday'  # Adding the duration of the trip
-}
-
-# Assistant guidelines and formatting template
-assistant_guidelines = """## Guidelines for creating the itinerary
----------------------------------------
-- Follow this structure for each day:
-  1. **Day X: [Weekday]**
-  ---------------------------------------
-    - **[Time] - [Activity Description]** (Provide a detailed explanation for why each activity is chosen.)
-        - *Cost*: [Cost Estimate]
-        - *Time Needed*: [Time Estimate]
-        - *Navigation*: [Exact Address or Navigation Details]
-        - *Insider Tip*: [If applicable]
-- Be extremely specific and actionable. 
-- Do not suggest 'maybe' or 'you could try' activities. Only include confirmed, planned items.
-- If unable to find information, suggest a specific alternative.
----------------------------------------
-## Cost Summary and Summarization
-- Include a cost summary and summarization at the end of the itinerary.
-"""
-
-print("API Call...")
-# GPT-3.5 Turbo API call
 
 # Function to call OpenAI API and process text
 def call_openai_api():
@@ -69,25 +18,13 @@ def call_openai_api():
         role = "You will be my personal travel guide to provide me with a tailor made travel plan"
         
         response = openai.ChatCompletion.create(
-    model=model,
-    messages=[
-        {"role": "system", "content": role},
-        {"role": "assistant", "content": assistant_guidelines},
-        {
-            "role": "user",
-            "content": f"I am a {static_params['age']}-year-old {static_params['gender']} visiting {varying_params['destination']} from {varying_params['duration']}. I am a {static_params['travel_type']} traveler on a {static_params['budget']}, staying {varying_params['location']}, and am {varying_params['transport']}."
-        },
-        {
-            "role": "user",
-            "content": f"Create an itinerary where each day features at least one activity that aligns with my interests: {', '.join(interests)}. Elaborate on why each activity is chosen and aligns with my interests. Also, include a cost summary and a summarization at the end of the itinerary."
-        }
-    ],
-    temperature=0,
-    max_tokens=token,
-    top_p=0.9,
-    frequency_penalty=0.5,
-    presence_penalty=0.6
-)
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": role},
+                {"role": "user", "content": prompt_text},
+                {"role": "assistant", "content": ""}
+            ]
+        )
         
         assistant_message = response['choices'][0]['message']['content']
         return assistant_message
