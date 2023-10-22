37ce13132b69f619205ada4792e8baa143aaf8a9
Jasper Aden <128797567+jaaden02@users.noreply.github.com>
2023-10-22 13:06:11 +0200

    try2

--- Webhook.py
+++ Webhook.py
@@ -1,49 +1,42 @@
-
 import openai
-import requests
-import json
 import os
 from flask import Flask, request, jsonify
 
 # Initialize Flask app
 app = Flask(__name__)
 
+#openai.api_key='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'
+
 # Function to call OpenAI API and process text
 def call_openai_api():
-    # Replace 'your-api-key' with your actual API key, ideally from an environment variable
-    #openai.api_key = os.environ.get('OPENAI_API_KEY')
+    try:
+        openai.api_key = os.environ.get('OPENAI_API_KEY', 'your-fallback-api-key')
+        
+        if not openai.api_key:
+            return jsonify({"error": "API key not found"}), 500
+        
+        prompt_text = 'i am going to visit Berlin'
+        role = "You will be my personal travel guide to provide me with a tailor made travel plan"
+        
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": role},
+                {"role": "user", "content": prompt_text},
+                {"role": "assistant", "content": ""}
+            ]
+        )
+        
+        assistant_message = response['choices'][0]['message']['content']
+        return assistant_message
     
-    openai.api_key='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'
-
-    if not openai.api_key:
-        return jsonify({"error": "API key not found"}), 500
-
-    # The prompt you want the model to complete
-    prompt_text = 'i am going to visit Berlin'
-    role = "You will be my personal travel guide to provide me with a tailor made travel plan"
-    
-    # The model you want to use, eg. 'text-davinci-003' for GPT-3
-    
-    response = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-        {"role": "system", "content": role},
-        {"role": "user", "content": prompt_text},
-        {"role": "assistant", "content": ""}
-      ]
-    )
-    
-    
-    assistant_message = response['choices'][0]['message']['content']
-    return assistant_message
-    
+    except Exception as e:
+        return jsonify({"error": f"An error occurred: {e}"}), 500
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     processed_text = call_openai_api()
     return jsonify({'processed_text': processed_text})
-    #return processed_text
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(debug=False)
