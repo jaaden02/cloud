6e1264efc34f51027eea31a68d3d20f71c6cd201
Jasper Aden <128797567+jaaden02@users.noreply.github.com>
2023-10-24 13:53:16 +0200

    long

--- Webhook.py
+++ Webhook.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 
 
 # Function to call OpenAI API and process text
+
+
+# GPT-3.5 Turbo API call
+
 def call_openai_api():
     try:
         openai.api_key = os.environ.get('OPENAI_API_KEY', 'your-fallback-api-key')
@@ -14,16 +18,73 @@ def call_openai_api():
         if not openai.api_key:
             return jsonify({"error": "API key not found"}), 500
         
-        prompt_text = 'i am going to visit Berlin'
-        role = "You will be my personal travel guide to provide me with a tailor made travel plan"
-        
+        model= "gpt-3.5-turbo-16k"
+        token= 16000
+
+        # Role definition
+        role = "You are a personal tour guide responsible for creating a detailed, all-inclusive, step-by-step itinerary."
+
+        # Static Parameters (Personal Profile, Travel Preferences, etc.)
+        static_params = {
+            'age': 21,
+            'gender': 'male',
+            'relationship_status': 'single',
+            'travel_companions': 'none',
+            'travel_type': 'solo',
+            'budget': 'student budget',
+            'activity_level': 'active'
+        }
+
+        # Interests
+        interests = ['independent films', 'techno music', 'vintage items', 'small cafes', 'old rustic bars']
+
+        # Varying Parameters (Specific to each trip)
+        varying_params = {
+            'destination': 'Berlin',
+            'location': 'near S Warschauer Brücke',
+            'transport': 'comfortable using public transit',
+            'duration': 'Thursday to Sunday'  # Adding the duration of the trip
+        }
+
+        # Assistant guidelines and formatting template
+        assistant_guidelines = """## Guidelines for creating the itinerary
+        ---------------------------------------
+        - Follow this structure for each day:
+        1. **Day X: [Weekday]**
+        ---------------------------------------
+            - **[Time] - [Activity Description]** (Provide a detailed explanation for why each activity is chosen.)
+                - *Cost*: [Cost Estimate]
+                - *Time Needed*: [Time Estimate]
+                - *Navigation*: [Exact Address or Navigation Details]
+                - *Insider Tip*: [If applicable]
+        - Be extremely specific and actionable. 
+        - Do not suggest 'maybe' or 'you could try' activities. Only include confirmed, planned items.
+        - If unable to find information, suggest a specific alternative.
+        ---------------------------------------
+        ## Cost Summary and Summarization
+        - Include a cost summary and summarization at the end of the itinerary.
+        """
+
+        print("API Call...")
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": role},
-                {"role": "user", "content": prompt_text},
-                {"role": "assistant", "content": ""}
-            ]
+        model=model,
+        messages=[
+            {"role": "system", "content": role},
+            {"role": "assistant", "content": assistant_guidelines},
+            {
+                "role": "user",
+                "content": f"I am a {static_params['age']}-year-old {static_params['gender']} visiting {varying_params['destination']} from {varying_params['duration']}. I am a {static_params['travel_type']} traveler on a {static_params['budget']}, staying {varying_params['location']}, and am {varying_params['transport']}."
+            },
+            {
+                "role": "user",
+                "content": f"Create an itinerary where each day features at least one activity that aligns with my interests: {', '.join(interests)}. Elaborate on why each activity is chosen and aligns with my interests. Also, include a cost summary and a summarization at the end of the itinerary."
+            }
+            ],
+            temperature=0,
+            max_tokens=token,
+            top_p=0.9,
+            frequency_penalty=0.5,
+            presence_penalty=0.6
         )
         
         assistant_message = response['choices'][0]['message']['content']
