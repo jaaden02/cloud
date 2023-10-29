88148414bb10181fb28e7acc491f798d50cfd5d9
Jasper Aden <128797567+jaaden02@users.noreply.github.com>
2023-10-29 12:33:38 +0100

    longer version timeout try

--- Webhook.py
+++ Webhook.py
@@ -1,87 +1,122 @@
-import openai
-import os
 from flask import Flask, request, jsonify
-import sys
+import threading
+import openai
+import time
 
 # Initialize Flask app
 app = Flask(__name__)
 
-# Function to handle streaming from OpenAI API
-def handle_stream(response):
-    collected_message = ""
-    for chunk in response:
-        delta = chunk.get('choices', [{}])[0].get('delta', {})
-        content = delta.get('content', None)
+# Event object to signal when API call is done
+api_done_event = threading.Event()
 
-        if content is not None:
-            print(content, end="")
-            sys.stdout.flush()
-            collected_message += content
-    return collected_message
-
-# Function to call OpenAI API and process text
+# GPT-3.5 Turbo API call
 def call_openai_api():
+
+
     try:
         openai.api_key = os.environ.get('OPENAI_API_KEY', 'your-fallback-api-key')
-        
+
         if not openai.api_key:
             return jsonify({"error": "API key not found"}), 500
         
-        prompt_text = (
-    "Title: 'The Dance of the Cosmos: Exploring the Interplay between Chaos Theory "
-    "and Quantum Mechanics in Predicting Celestial Mechanics'\n\n"
-    "Prompt:\n\n"
-    "The deterministic yet unpredictable nature of chaotic systems and the inherent "
-    "uncertainty in quantum mechanics appear to be at odds, yet they both play crucial "
-    "roles in understanding the behavior of celestial bodies. Your task is to delve into "
-    "the interplay between Chaos Theory and Quantum Mechanics in the realm of celestial mechanics.\n\n"
-    "1. Provide a comprehensive overview of Chaos Theory and Quantum Mechanics, elucidating "
-    "their fundamental principles, mathematical frameworks, and historical evolution.\n"
-    "2. Discuss how these theories have been utilized or juxtaposed in explaining and predicting "
-    "the dynamics of celestial bodies, including planets, asteroids, comets, and other astronomical phenomena.\n"
-    "3. Explore the challenges and limitations faced by physicists and astronomers in employing "
-    "these theories for celestial predictions. How has the advent of quantum computing and advancements "
-    "in numerical simulations aided in overcoming these challenges?\n"
-    "4. Delve into speculative or emerging theories that attempt to bridge the gaps between Chaos Theory "
-    "and Quantum Mechanics, and discuss their potential implications for future astronomical predictions "
-    "and explorations.\n"
-    "5. Reflect on the philosophical and practical implications of the interplay between determinism and "
-    "uncertainty in our understanding of the universe. How does this interplay influence the scientific "
-    "quest for a unified theory of physics?\n"
-    "6. Finally, propose a hypothetical or real-world scenario where a melding of Chaos Theory and Quantum "
-    "Mechanics could significantly advance our understanding or prediction of celestial mechanics. Discuss "
-    "the potential impact on space exploration, astrophysical research, or technological advancements.\n\n"
-    "Ensure your response is well-researched, thoroughly referenced, and delves deeply into the mathematical "
-    "and physical intricacies involved, making connections to real-world applications and speculative future scenarios."
-)
+        model= "gpt-3.5-turbo-16k"
+        token= 16000
 
-        role = "You will be my physics teacher"
-        
+        # Role definition
+        role = "You are a personal tour guide responsible for creating a detailed, all-inclusive, step-by-step itinerary."
+
+        # Static Parameters (Personal Profile, Travel Preferences, etc.)
+        static_params = {
+            'age': 21,
+            'gender': 'male',
+            'relationship_status': 'single',
+            'travel_companions': 'none',
+            'travel_type': 'solo',
+            'budget': 'student budget',
+            'activity_level': 'active'
+        }
+
+        # Interests
+        interests = ['independent films', 'techno music', 'vintage items', 'small cafes', 'old rustic bars']
+
+        # Varying Parameters (Specific to each trip)
+        varying_params = {
+            'destination': 'Berlin',
+            'location': 'near S Warschauer Brücke',
+            'transport': 'comfortable using public transit',
+            'duration': 'Thursday to Sunday'  # Adding the duration of the trip
+        }
+
+        # Assistant guidelines and formatting template
+        assistant_guidelines = """## Guidelines for creating the itinerary
+        ---------------------------------------
+        - Follow this structure for each day:
+        1. **Day X: [Weekday]**
+        ---------------------------------------
+            - **[Time] - [Activity Description]** (Provide a detailed explanation for why each activity is chosen.)
+                - *Cost*: [Cost Estimate]
+                - *Time Needed*: [Time Estimate]
+                - *Navigation*: [Exact Address or Navigation Details]
+                - *Insider Tip*: [If applicable]
+        - Be extremely specific and actionable. 
+        - Do not suggest 'maybe' or 'you could try' activities. Only include confirmed, planned items.
+        - If unable to find information, suggest a specific alternative.
+        ---------------------------------------
+        ## Cost Summary and Summarization
+        - Include a cost summary and summarization at the end of the itinerary.
+        """
+
+        print("API Call...")
         response = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=[
-                {"role": "system", "content": role},
-                {"role": "user", "content": prompt_text},
-                {"role": "assistant", "content": ""}
+        model=model,
+        messages=[
+            {"role": "system", "content": role},
+            {"role": "assistant", "content": assistant_guidelines},
+            {
+                "role": "user",
+                "content": f"I am a {static_params['age']}-year-old {static_params['gender']} visiting {varying_params['destination']} from {varying_params['duration']}. I am a {static_params['travel_type']} traveler on a {static_params['budget']}, staying {varying_params['location']}, and am {varying_params['transport']}."
+            },
+            {
+                "role": "user",
+                "content": f"Create an itinerary where each day features at least one activity that aligns with my interests: {', '.join(interests)}. Elaborate on why each activity is chosen and aligns with my interests. Also, include a cost summary and a summarization at the end of the itinerary."
+            }
             ],
-            max_tokens=7000,
-            stream=True  # Turn on streaming
+            temperature=0,
+            max_tokens=token,
+            top_p=0.9,
+            frequency_penalty=0.5,
+            presence_penalty=0.6
         )
-        
-        assistant_message = handle_stream(response)
-        
-        print("\nComplete Message:")
-        print(assistant_message)
-        
+        print("Finished API call.")
+        api_done_event.set()
+        assistant_message = response['choices'][0]['message']['content']
+        print("API Response:", assistant_message)  # Print the API response
         return assistant_message
-    
+
     except Exception as e:
         return jsonify({"error": f"An error occurred: {e}"}), 500
 
+def occupy_system():
+    print("System is being occupied.")
+    while not api_done_event.is_set():
+        print("Occupying...")
+        time.sleep(1)
+    print("System is free now.")
+
 @app.route('/Webhook', methods=['POST'])
 def webhook():
-    processed_text = call_openai_api()
-    return jsonify({'processed_text': processed_text})
+    api_thread = threading.Thread(target=call_openai_api)
+    occupy_thread = threading.Thread(target=occupy_system)
+
+    api_thread.start()
+    occupy_thread.start()
+
+    api_thread.join()
+    occupy_thread.join()
+
+    api_done_event.clear()
+
+    return jsonify({'status': 'accepted'}), 202
 
 if __name__ == '__main__':
     app.run(debug=False)
