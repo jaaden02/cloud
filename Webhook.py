a63edd84d1d57b50002c6e99a1d56b3224b0a1d2
Jasper Aden <128797567+jaaden02@users.noreply.github.com>
2023-11-18 08:00:44 +0100

    back to long

--- Webhook.py
+++ Webhook.py
@@ -1,20 +1,116 @@
+import openai
+import os
 from flask import Flask, request, jsonify
+import sys
 
+# Initialize Flask app
 app = Flask(__name__)
 
-@app.route('/LocalFlask', methods=['POST'])
+'''# Function to handle streaming from OpenAI API
+def handle_stream(response):
+    collected_message = ""
+    for chunk in response:
+        delta = chunk.get('choices', [{}])[0].get('delta', {})
+        content = delta.get('content', None)
+
+        if content is not None:
+            print(content, end="")
+            sys.stdout.flush()
+            collected_message += content
+    return collected_message'''
+
+# Function to call OpenAI API and process text
+def call_openai_api():
+    try:
+        openai.api_key = os.environ.get('OPENAI_API_KEY', 'your-fallback-api-key')
+
+        if not openai.api_key:
+            return jsonify({"error": "API key not found"}), 500
+        
+        model= "gpt-3.5-turbo-16k"
+        token= 16000
+
+        # Role definition
+        role = "You are a personal tour guide responsible for creating a detailed, all-inclusive, step-by-step itinerary."
+
+        # Static Parameters (Personal Profile, Travel Preferences, etc.)
+        static_params = {
+            'age': 21,
+            'gender': 'male',
+            'relationship_status': 'single',
+            'travel_companions': 'none',
+            'travel_type': 'solo',
+            'budget': 'student budget',
+            'activity_level': 'active'
+        }
+
+        # Interests
+        interests = ['independent films', 'techno music', 'vintage items', 'small cafes', 'old rustic bars']
+
+        # Varying Parameters (Specific to each trip)
+        varying_params = {
+            'destination': 'Berlin',
+            'location': 'near S Warschauer Brücke',
+            'transport': 'comfortable using public transit',
+            'duration': 'Thursday to Sunday'  # Adding the duration of the trip
+        }
+
+        # Assistant guidelines and formatting template
+        assistant_guidelines = """## Guidelines for creating the itinerary
+        ---------------------------------------
+        - Follow this structure for each day:
+        1. **Day X: [Weekday]**
+        ---------------------------------------
+            - **[Time] - [Activity Description]** (Provide a detailed explanation for why each activity is chosen.)
+                - *Cost*: [Cost Estimate]
+                - *Time Needed*: [Time Estimate]
+                - *Navigation*: [Exact Address or Navigation Details]
+                - *Insider Tip*: [If applicable]
+        - Be extremely specific and actionable. 
+        - Do not suggest 'maybe' or 'you could try' activities. Only include confirmed, planned items.
+        - If unable to find information, suggest a specific alternative.
+        ---------------------------------------
+        ## Cost Summary and Summarization
+        - Include a cost summary and summarization at the end of the itinerary.
+        """
+
+        print("API Call...")
+        response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": role},
+            {"role": "assistant", "content": assistant_guidelines},
+            {
+                "role": "user",
+                "content": f"I am a {static_params['age']}-year-old {static_params['gender']} visiting {varying_params['destination']} from {varying_params['duration']}. I am a {static_params['travel_type']} traveler on a {static_params['budget']}, staying {varying_params['location']}, and am {varying_params['transport']}."
+            },
+            {
+                "role": "user",
+                "content": f"Create an itinerary where each day features at least one activity that aligns with my interests: {', '.join(interests)}. Elaborate on why each activity is chosen and aligns with my interests. Also, include a cost summary and a summarization at the end of the itinerary."
+            }
+            ],
+            temperature=0,
+            max_tokens=token,
+            top_p=0.9,
+            frequency_penalty=0.5,
+            presence_penalty=0.6,
+            #stream = True
+            
+        )
+        
+        assistant_message = response['choices'][0]['message']['content']
+        return assistant_message
+    
+    except Exception as e:
+        return jsonify({"error": f"An error occurred: {e}"}), 500
+
+@app.route('/Webhook', methods=['POST'])
 def webhook():
-    print("Request Headers:", request.headers)
-    data = request.json
-    print("Received Payload:", data)
-    # Your existing response code...
-    response = {
-        "processed_text": "**Day 3: Saturday**\n\n- **10:00 AM - Breakfast at Commonground**\n    - *Cost*: €10-15\n    - *Time Needed*: 1 hour\n    - *Navigation*: Rosenthaler Str. 1, 10119 Berlin\n    - *Insider Tip*: Commonground is a trendy café known for its delicious breakfast options and stylish interior. Start your day with a satisfying meal before embarking on your vintage shopping adventure.\n\n- **11:30 AM - Vintage Shopping in Prenzlauer Berg**\n    - *Cost*: Free (unless you decide to purchase something)\n    - *Time Needed*: 3 hours\n    - *Navigation*: Kastanienallee, Oderberger Str., and surrounding streets in Prenzlauer Berg\n    - *Insider Tip*: Prenzlauer Berg is a neighborhood known for its vintage shops and boutiques. Explore the charming streets and discover unique clothing, accessories, and retro treasures.\n\n- **2:30 PM - Lunch at Prater Biergarten**\n    - *Cost*: €10-15\n    - *Time Needed*: 1 hour\n    - *Navigation*: Kastanienallee 7-9, 10435 Berlin\n    - *Insider Tip*: Prater Biergarten is a historic beer garden offering traditional German cuisine. Enjoy a hearty meal and soak up the relaxed atmosphere before continuing your exploration.\n\n- **4:00 PM - Visit Boxhagener Platz Flohmarkt**\n    - *Cost*: Free (unless you decide to purchase something)\n    - *Time Needed*: 2 hours\n    - *Navigation*: Boxhagener Platz, 10245 Berlin\n    - *Insider Tip*: The Boxhagener Platz Flohmarkt is a popular flea market where you can find a wide range of vintage items, antiques, and second-hand goods. Spend some time browsing through the stalls and bargaining for unique finds.\n\n- **6:30 PM - Dinner at Schwarzwaldstuben**\n    - *Cost*: €15-20\n    - *Time Needed*: 1 hour\n    - *Navigation*: Tucholskystraße 48, 10117 Berlin\n    - *Insider Tip*: Schwarzwaldstuben is a cozy restaurant serving traditional German dishes with a modern twist. Indulge in their delicious food while enjoying the rustic ambiance of this hidden gem.\n"
-    }
-    return jsonify(response)
+    processed_text = call_openai_api()
+    return jsonify({'processed_text': processed_text})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=5000)
+    app.run(debug=False)
 
 
     #
